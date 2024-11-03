3010e71b4ffcda81d63d8de83f8c4a8bea2312f6
Neon-20 <rajveer23pr@gmail.com>
2024-11-03 12:24:33 +0530

    feat: sidebar and Card mock

--- app/(main)/realtime/page.tsx
+++ app/(main)/realtime/page.tsx
@@ -1,3 +1,4 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import RealtimeVoiceTranscription from "@/components/VoiceGPT";
 
 export default function DashBoardPage() {
@@ -5,12 +6,12 @@ export default function DashBoardPage() {
     <div className="flex flex-col bg-[#0e0e0e] overflow-y-auto h-[100vh]">
       <main className="flex-1 bg-[#ffffff07] text-white p-8 m-8 rounded-2xl mr-5">
         <h2 className="text-2xl font-bold mb-8">Overview</h2>
-        <div className="grid grid-cols-2 gap-8">
-          <div className="bg-[#ffffff07] border border-white/5 rounded-2xl shadow-lg shadow-black/10 flex flex-col justify-between flex-1 min-w-[300px] p-4 pt-1">
-            <h1>GPT-4 Voice Interaction</h1>
-            <RealtimeVoiceTranscription />
-          </div>
-        </div>
+        <Card className="w-96 h-32 bg-[#ffffff07] border-none">
+        <CardHeader>
+        <CardTitle className="text-white">GPT-4o Interaction</CardTitle>
+        <CardDescription>Start</CardDescription>
+      </CardHeader>
+        </Card>
       </main>
     </div>
   );
